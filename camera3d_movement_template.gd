b4a9b6587cb91fb7a2ef075487b792db067265f2
William Torpey <54170423+JohnnyThunder2@users.noreply.github.com>
2024-09-24 23:51:45 -0700

    Update camera3d_movement_template.gd

--- camera3d_movement_template.gd
+++ camera3d_movement_template.gd
@@ -4,7 +4,7 @@
 ##	This script is to help Game Jams or people who hate wasteing time by giving GameDevs 
 ##      a quick script for setting up basic Camera3d controls in a new project
 ##	Just add Keybinds for: Camera_Forward, Camera_Back, Camera_Right, Camera_Left, Camera_Up, 
-##	Camera_Down, Sprint and Free_Look
+##	Camera_Down, Sprint, Quit and Free_Look
 ##	Linux Install location: $HOME/.config/godot/script_templates/Camera3D/
 ##      MacOS Install location: $HOME/Library/Application Support/Godot/script_templates/Camera3D/
 ##      Windows Install Location: %APPDATA%\Godot\script_templates\Camera3D\
